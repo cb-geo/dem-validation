e191b5e37759f99ab567ab55783d67137c14d2d3
Krishna Kumar <kks32@cam.ac.uk>
2017-09-08 15:03:17 +0100

    :computer: Refactor lammps test script

--- ci/compile.sh
+++ ci/compile.sh
@@ -4,12 +4,14 @@ rm -f *.dump
 make clean-all
 make yes-granular |& tee log.make_yes_granular
 make -j serial |& tee log.make_serial
-/lammps/src/lmp_serial -i /lammps-dem-validation/contact/in.linear_withoutDamp.planeParticle &&
-/lammps/src/lmp_serial -i /lammps-dem-validation/contact/in.hertz_withoutDamp.planeParticle &&
-/lammps/src/lmp_serial -i /lammps-dem-validation/contact/in.linear_withoutDamp.twoparticles &&
-/lammps/src/lmp_serial -i /lammps-dem-validation/rolling/in.granlinear.rolling_sphere &&
-/lammps/src/lmp_serial -i /lammps-dem-validation/rolling/in.granhertz.rolling_sphere
-diff  -q /lammps/src/hertz_withoutDamp_hertz.dump /lammps-dem-validation/contact/hertz_withoutDamp_hertz.dump 1>/dev/null
+lammpslocation=$1
+validationfiles=$2
+$lammpslocation/lmp_serial -i $validationfiles/contact/in.linear_withoutDamp.planeParticle &&
+$lammpslocation/lmp_serial -i $validationfiles/contact/in.hertz_withoutDamp.planeParticle &&
+$lammpslocation/lmp_serial -i $validationfiles/contact/in.linear_withoutDamp.twoparticles &&
+$lammpslocation/lmp_serial -i $validationfiles/rolling/in.granlinear.rolling_sphere &&
+$lammpslocation/lmp_serial -i $validationfiles/rolling/in.granhertz.rolling_sphere
+diff  -q $lammpslocation/hertz_withoutDamp_hertz.dump $validationfiles/contact/hertz_withoutDamp_hertz.dump 1>/dev/null
 if [[ $? == "0" ]]
 then
   echo "Test results are same"
