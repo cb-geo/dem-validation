3c3c90adf1780938a15dedb767eb347e1bd772ae
Krishna Kumar <kks32@cam.ac.uk>
2017-09-08 09:39:11 +0100

    :dart: Fixes #9 Added test cases to Granular LAMMPS

--- singularity-lammps/README.md
+++ singularity-lammps/README.md
@@ -1,5 +1,10 @@
 # Running GranLammps on CI stormfly
 
 ```
-singularity run --bind $(pwd):/lammps --bind /home/jenkins/lammps-run:lammps-run /home/jenkins/centos.img /lammps-run/compile.sh
+singularity run \
+	--bind $(pwd):/lammps \
+	--bind /home/jenkins/lammps-run:lammps-run \
+	--bind /home/jenkins/lammps-dem-validation:lammps-dem-validation \
+	/home/jenkins/centos.img \
+	/lammps-run/compile.sh
 ```
--- singularity-lammps/compile.sh
+++ singularity-lammps/compile.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 cd /lammps/src
+make clean-all
 make yes-granular |& tee log.make_yes_granular
 make -j serial |& tee log.make_serial
+/lammps/src/lmp_serial -i /lammps-dem-validation/contact/in.linear_withoutDamp.planeParticle &&
+/lammps/src/lmp_serial -i /lammps-dem-validation/contact/in.linear_withoutDamp.planeParticle &&
+/lammps/src/lmp_serial -i /lammps-dem-validation/contact/in.linear_withoutDamp.twoparticles &&
+/lammps/src/lmp_serial -i /lammps-dem-validation/rolling/in.granlinear.rolling_sphere &&
+/lammps/src/lmp_serial -i /lammps-dem-validation/rolling/in.granhertz.rolling_sphere
